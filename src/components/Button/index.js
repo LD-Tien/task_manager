b07167919b3c7c51a096bd2e0c582aadeba99215
LD-Tien <leductien00@gmail.com>
2023-09-21 21:43:32 +0700

    Update UI Button component: Add style item

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,14 +1,38 @@
-import styles from "./Button.module.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./Button.module.scss";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
-
-function Button({ leftIcon, rightIcon, children }) {
+function Button({
+  leftIcon,
+  rightIcon,
+  item,
+  small,
+  href,
+  isActive,
+  isFile,
+  children,
+  onClick,
+  onClickCancel,
+}) {
   let Comp = "button";
+  if (!!href) {
+    Comp = "a";
+  }
   return (
-    <div className={styles["wrapper"]}>
-      <Comp>
+    <div
+      className={`${styles["wrapper"]} ${item ? styles["item"] : ""} ${
+        small ? styles["small"] : ""
+      } ${isActive ? styles["active"] : ""} ${isFile ? styles["file"] : ""}`}
+    >
+      <Comp href={!!href && href} target={!!href && "_blank"}>
         {leftIcon && <span className={styles["leftIcon"]}>{leftIcon}</span>}
         {children && <span className={styles["text"]}>{children}</span>}
         {rightIcon && <span className={styles["rightIcon"]}>{rightIcon}</span>}
+        {(isActive || isFile) && (
+          <span className={styles["cancel"]} onClick={onClickCancel}>
+            {<FontAwesomeIcon icon={faXmark} style={{ color: "#a19f9d" }} />}
+          </span>
+        )}
       </Comp>
     </div>
   );
