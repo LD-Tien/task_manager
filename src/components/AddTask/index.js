11094e9e8e70c25ff0c3b643be5add6513437c55
LD-Tien <leductien00@gmail.com>
2023-09-25 16:02:04 +0700

    Build UI AddTask component

--- src/components/AddTask/index.js
+++ src/components/AddTask/index.js
@@ -1,26 +1,53 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./AddTask.module.scss";
-import { faCalendar, faClock } from "@fortawesome/free-regular-svg-icons";
+import {
+  faBell,
+  faCalendar,
+} from "@fortawesome/free-regular-svg-icons";
 import { faPlus, faRepeat } from "@fortawesome/free-solid-svg-icons";
 import Button from "../Button";
+import { default as MenuPopper } from "../Popper/Menu";
+import { DUE_MENU_POPPER, REPEAT_MENU_POPPER } from "../../store/constraints";
 
 function AddTask({ planOptions }) {
   return (
-    <>
-      <div className={styles["wrapper"]}>
-        <FontAwesomeIcon icon={faPlus} />
-        <input type="text" placeholder="Add a task" />
-        {planOptions ? (
-          <>
-            <Button leftIcon={<FontAwesomeIcon icon={faClock} />} small/>
-            <Button leftIcon={<FontAwesomeIcon icon={faCalendar} />} small/>
-            <Button leftIcon={<FontAwesomeIcon icon={faRepeat} />} small/>
-          </>
-        ) : (
-          ""
-        )}
-      </div>
-    </>
+    <div className={styles["wrapper"]}>
+      <FontAwesomeIcon icon={faPlus} />
+      <input type="text" placeholder="Add a task" />
+      {planOptions ? (
+        <div className={styles["plan-options"]}>
+          <MenuPopper
+            trigger="click"
+            placement="bottom"
+            items={DUE_MENU_POPPER}
+          >
+            <div>
+              <Button leftIcon={<FontAwesomeIcon icon={faCalendar} />} small />
+            </div>
+          </MenuPopper>
+          <MenuPopper
+            trigger="click"
+            placement="bottom"
+            items={DUE_MENU_POPPER}
+          >
+            <div>
+              <Button leftIcon={<FontAwesomeIcon icon={faBell} />} small />
+            </div>
+          </MenuPopper>
+          <MenuPopper
+            trigger="click"
+            placement="bottom"
+            items={REPEAT_MENU_POPPER}
+          >
+            <div>
+              <Button leftIcon={<FontAwesomeIcon icon={faRepeat} />} small />
+            </div>
+          </MenuPopper>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
   );
 }
 
