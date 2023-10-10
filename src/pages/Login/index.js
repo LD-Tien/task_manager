16454bc578a348ef181f2fb713f6d9ec13039fe2
LD-Tien <leductien00@gmail.com>
2023-10-10 22:44:03 +0700

    Add validate and perf code

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useLayoutEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLock } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
@@ -7,50 +7,61 @@ import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import styles from "./Login.module.scss";
 import TextInput from "../../components/TextInput";
 import Button from "../../components/Button";
+import User from "../../models/User";
 
 function Login() {
   const [emailLogin, setEmailLogin] = useState("");
   const [passwordLogin, setPasswordLogin] = useState("");
   const [errorTextLogin, setErrorTextLogin] = useState("");
   const [isLogged, setIsLogged] = useState(true);
-  const navigate = useNavigate();
 
-  function handleClickLogin(e) {
+  async function handleClickLogin(e) {
     e.preventDefault();
-
-    fetch("/login", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({ email: emailLogin, password: passwordLogin }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (!data.errorMessage) {
-          // login successfully
-          setErrorTextLogin("");
-          document.cookie = "TMToken = " + data.token;
-          navigate("/home");
-        } else {
-          // login failed
-          setErrorTextLogin(data.errorMessage);
-        }
-      });
+    if(!validateEmail(emailLogin)) {
+      setErrorTextLogin("Please enter the correct email format")
+      return;
+    }
+    if(passwordLogin.length < 4) {
+      setErrorTextLogin("Password must have at least 4 characters")
+      return;
+    }
+    const user = new User({
+      email: emailLogin,
+      password: passwordLogin,
+      username: emailLogin.split("@")[0],
+    });
+    const data = await user.login();
+    if (!data.errorMessage) {
+      // login successfully
+      setErrorTextLogin("");
+      document.cookie = "TMToken = " + data.token;
+      window.location.replace("/home");
+    } else {
+      setErrorTextLogin(data.errorMessage);
+    }
   }
 
-  useEffect(() => {
-    fetch("/checkLoginToken")
-      .then((res) => res.json())
-      .then((data) => {
-        if (data) {
-          navigate("/home");
-          setIsLogged(true);
-        } else {
-          setIsLogged(false);
-        }
-      });
-  }, [navigate]);
+  function validateEmail(email) {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  }
+
+  async function checkLogin() {
+    const result = await new User().auth();
+    if (result) {
+      window.location.replace("/home");
+      setIsLogged(true);
+    } else {
+      setIsLogged(false);
+    }
+  }
+
+  useLayoutEffect(() => {
+    checkLogin();
+  }, []);
 
   return !isLogged ? (
     <div className={styles["wrapper"]}>
@@ -77,7 +88,7 @@ function Login() {
         </Button>
         <p className={styles["error-text"]}>{errorTextLogin}</p>
         <hr />
-        <Link className={styles["link"]} to={"/signup"}>
+        <Link className={styles["link"]} to={"/signup"} replace>
           Create account
         </Link>
       </form>
--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -2,59 +2,78 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import TextInput from "../../components/TextInput";
 import styles from "./Signup.module.scss";
 import Button from "../../components/Button";
-import { useEffect, useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import {
   faCircleCheck,
   faCircleXmark,
   faEnvelope,
 } from "@fortawesome/free-regular-svg-icons";
 import { faLock } from "@fortawesome/free-solid-svg-icons";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import User from "../../models/User";
 
 function Signup() {
   const [emailSignup, setEmailSignup] = useState("");
   const [passwordSignup, setPasswordSignup] = useState("");
   const [retypePasswordSignup, setRetypePasswordSignup] = useState("");
+  const [errorTextSignup, setErrorTextSignup] = useState("");
   const [isLogged, setIsLogged] = useState(true);
-  const navigate = useNavigate();
 
-  function handleClickSignup(e) {
+  async function handleClickSignup(e) {
     e.preventDefault();
+    e.preventDefault();
+    if (!validateEmail(emailSignup)) {
+      setErrorTextSignup("Please enter the correct email format");
+      return;
+    }
+    if (passwordSignup.length < 4) {
+      setErrorTextSignup("Password must have at least 4 characters");
+      return;
+    }
+    if (!(retypePasswordSignup === passwordSignup)) {
+      setErrorTextSignup("Re-enter password does not match password");
+      return;
+    }
 
-    fetch("/signup", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({ email: emailSignup, password: passwordSignup }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (!data.errorMessage) {
-          // signup successfully
-          document.cookie = "TMToken = " + data.token;
-          navigate("/home")
-          setErrorTextSignup("");
-        } else {
-          // login failed
-          setErrorTextSignup(data.errorMessage);
-        }
-      });
+    const user = new User({
+      email: emailSignup,
+      password: passwordSignup,
+      username: emailSignup.split("@")[0],
+    });
+
+    const result = await user.signup();
+    if (!result.errorMessage) {
+      // signup successfully
+      document.cookie = "TMToken = " + result.token;
+      window.location.replace("/home");
+      setErrorTextSignup("");
+    } else {
+      // login failed
+      setErrorTextSignup(result.errorMessage);
+    }
   }
 
-  useEffect(() => {
-    fetch("/checkLoginToken")
-      .then((res) => res.json())
-      .then((data) => {
-        if (data) {
-          navigate("/home");
-          setIsLogged(true);
-        } else {
-          setIsLogged(false);
-        }
-      });
-  }, [navigate]);
-  const [errorTextSignup, setErrorTextSignup] = useState("");
+  function validateEmail(email) {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  }
+
+  async function checkLogin() {
+    const result = await new User().auth();
+    if (result) {
+      window.location.replace("/home");
+      setIsLogged(true);
+    } else {
+      setIsLogged(false);
+    }
+  }
+
+  useLayoutEffect(() => {
+    checkLogin();
+  }, []);
   return !isLogged ? (
     <div className={styles["wrapper"]}>
       <form>
@@ -93,7 +112,7 @@ function Signup() {
         </Button>
         <p className={styles["error-text"]}>{errorTextSignup}</p>
         <hr />
-        <Link className={styles["link"]} to={"/login"}>
+        <Link className={styles["link"]} to={"/login"} replace>
           Already have a account
         </Link>
       </form>
